5d895c85682879b6050f1d2bda5f681210313a5b
bitsnaps <bitsnaps@yahoo.fr>
2021-07-31 23:29:32 +0000

    update deprecated code

--- android/app/src/main/java/io/flutter/examples/stocks/MainActivity.java
+++ android/app/src/main/java/io/flutter/examples/stocks/MainActivity.java
@@ -5,13 +5,14 @@
 package io.flutter.examples.stocks;
 
 import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+/*/ Deprecated with new Flutter API
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
-  }
+  }*/
 }
--- android/app/src/main/kotlin/io/flutter/examples/flutter_stock_example/MainActivity.kt
+++ android/app/src/main/kotlin/io/flutter/examples/flutter_stock_example/MainActivity.kt
@@ -1,12 +1,14 @@
 package io.flutter.examples.flutter_stock_example
 
 import android.os.Bundle
-import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+/*/ Deprecated with new Flutter API
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         GeneratedPluginRegistrant.registerWith(this)
     }
+    */
 }
